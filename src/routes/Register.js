cb56f0bab192df6ade85832e8d3b4b3507f2619d
adamtowers <hello@adamtowers.design>
2018-05-29 18:08:20 -0400

    Styled login and register forms

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Container, Segment } from 'semantic-ui-react'
 
 class Register extends Component {
   constructor(props){
@@ -27,23 +28,27 @@ class Register extends Component {
 
   render() {
     return (
-      <div id="register">
-      <h1>Register</h1>
-        <form onSubmit={this.onSubmit.bind(this)} className="ui form">
-          <div className="field">
-            <label>Username</label>
-            <input type="text" name="username" onChange={this.handleChange} value={this.state.username} />
-          </div>
+      <Container text>
+        <Segment.Group>
+          <Segment>
+            <h1>Register</h1>
+              <form onSubmit={this.onSubmit.bind(this)} className="ui form">
+                <div className="field">
+                  <label>Username</label>
+                  <input type="text" name="username" onChange={this.handleChange} value={this.state.username} />
+                </div>
 
-          <div className="field">
-            <label>Password</label>
-            <input type="password" name="password" onChange={this.handleChange} value={this.state.password}/>
-          </div>
+                <div className="field">
+                  <label>Password</label>
+                  <input type="password" name="password" onChange={this.handleChange} value={this.state.password}/>
+                </div>
 
-          <button className="ui button" type="submit">Submit</button>
-        </form>
-        <button className="ui button" tabIndex="0" type="button" onClick={this.onLogin}>Login</button>
-      </div>
+                <button className="ui button" type="submit">Submit</button>
+                <button className="ui button" tabIndex="0" type="button" onClick={this.onLogin}>Login</button>
+              </form>
+            </Segment>
+          </Segment.Group>
+        </Container>
     )
   }
 }
