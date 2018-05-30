e6568e75e2634ee149ca1ce1c23cf649647e3ae1
Al <lin.alice72@gmail.com>
2018-05-30 14:10:30 -0400

    merge

--- src/components/RoomListContainer.js
+++ src/components/RoomListContainer.js
@@ -3,14 +3,6 @@ import RoomListItem from './RoomListItem'
 import 'semantic-ui-css/semantic.min.css'
 
 class RoomListContainer extends Component {
-<<<<<<< HEAD
-  constructor(props){
-    super(props)
-=======
-
-  componentDidMount() {
->>>>>>> 00b2e6e46895d042e12b544516112893c08961bc
-  }
 
   joinRoom = (e) => {
     if(localStorage.user_id && localStorage.user_id !== "" && e.target.id ){
